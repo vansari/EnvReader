7a9ea3475384b3203e1221c4538f3e8717efbb42
vansari <junker.kurt@googlemail.com>
2023-02-18 21:53:48 +0100

    check also that Class reads from $_ENV, $_SERVER and getenv

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -23,7 +23,7 @@ class EnvTest extends TestCase
     }
 
     #[DataProvider('provider_testGetWillPass')]
-    public function testGetWillPass(string $env, mixed $value, string $type, string $expected): void
+    public function testGetWillPassWithPutenv(?string $env, mixed $value, string $type, string $expected): void
     {
         putenv("$env=$value");
         $envValue = Env::getInstance()->get($env, $type);
@@ -31,8 +31,32 @@ class EnvTest extends TestCase
         putenv($env);
     }
 
+    #[DataProvider('provider_testGetWillPass')]
+    public function testGetWillPassWithENV(?string $env, mixed $value, string $type, string $expected): void
+    {
+        $_ENV[$env] = $value;
+        $envValue = Env::getInstance()->get($env, $type);
+        $this->assertSame($expected, $envValue);
+        unset($_ENV[$env]);
+    }
+
+    #[DataProvider('provider_testGetWillPass')]
+    public function testGetWillPassWithSERVER(?string $env, mixed $value, string $type, string $expected): void
+    {
+        $_SERVER[$env] = $value;
+        $envValue = Env::getInstance()->get($env, $type);
+        $this->assertSame($expected, $envValue);
+        unset($_SERVER[$env]);
+    }
+
+    public function testGetWillReturnNullIfNotExists(): void
+    {
+        $envValue = Env::getInstance()->get('NOT_EXISTS', 'string');
+        $this->assertNull($envValue);
+    }
+
     public function testGetWillFail(): void
     {
-
+        $this->markTestIncomplete();
     }
 }
