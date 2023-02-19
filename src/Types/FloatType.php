713597f73401c19e64ef10881a21cd7e8ba2ecdb
vansari <junker.kurt@googlemail.com>
2023-02-19 21:26:04 +0100

    added FloatType.php added FloatTypeTest.php

---
+++ src/Types/FloatType.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Freesoftde\EnvReader\Types;
+
+use Freesoftde\EnvReader\Exception\ConvertionException;
+
+class FloatType implements TypeInterface
+{
+
+    public function getName(): string
+    {
+        return 'float';
+    }
+
+    public function convert(mixed $value): float
+    {
+        if (false === ($filtered = filter_var($value, \FILTER_VALIDATE_FLOAT))) {
+            throw new ConvertionException("Could not convert value $value to float.");
+        }
+
+        return (float)$filtered;
+    }
+}
---
+++ tests/Types/FloatTypeTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Freesoftde\EnvReader\Test\Types;
+
+use Freesoftde\EnvReader\Exception\ConvertionException;
+use Freesoftde\EnvReader\Types\FloatType;
+use Freesoftde\EnvReader\Types\TypeCollection;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class FloatTypeTest extends TestCase
+{
+
+    public static function provider_testGet(): array
+    {
+        return [
+            [
+                '123.45',
+                123.45
+            ],
+            [
+                '1000',
+                1000.00
+            ],
+            [
+                '0.1234',
+                0.1234
+            ]
+        ];
+    }
+
+    #[DataProvider('provider_testGet')]
+    public function testGet(string $value, float $expected): void
+    {
+        $this->assertSame($expected, (new FloatType())->convert($value));
+    }
+
+    public function testGetWillFail(): void
+    {
+        $this->expectException(ConvertionException::class);
+        (new FloatType())->convert('abcdef');
+    }
+}
