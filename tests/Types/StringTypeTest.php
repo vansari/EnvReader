7c8ea1b2e0ff8bc16dddef1a403f248f9596cec7
vansari <junker.kurt@googlemail.com>
2023-02-19 20:24:51 +0100

    added StringTypeTest.php

---
+++ tests/Types/StringTypeTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Freesoftde\EnvReader\Test\Types;
+
+use Freesoftde\EnvReader\Types\StringType;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class StringTypeTest extends TestCase
+{
+
+    public static function provider_testGet(): array
+    {
+        // Todo: More TestCases
+        return [
+            ['text', 'text'],
+            [12345, '12345'],
+            [123.56, '123.56'],
+            [
+                new class implements \Stringable
+                {
+                    public function __toString(): string
+                    {
+                        return 'anonymous';
+                    }
+                },
+                'anonymous'
+            ]
+        ];
+    }
+
+    #[DataProvider('provider_testGet')]
+    public function testGet(mixed $input, string $expected): void
+    {
+        $this->assertSame($expected, (new StringType())->get($input));
+    }
+}
