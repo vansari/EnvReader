935e086478c7a009687400adbadad1562c15e8a7
vansari <kurt.junker@posteo.de>
2023-02-25 17:32:50 +0100

    5 - added Array Type and Tests

---
+++ src/Types/ArrayType.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace devcirclede\EnvReader\Types;
+
+use devcirclede\EnvReader\Exception\ConvertionException;
+
+class ArrayType implements TypeInterface
+{
+
+    public function getName(): string
+    {
+        return 'array';
+    }
+
+    public function convert(string $value): array
+    {
+        if (!preg_match('/^\[(?<values>.*)]$/', $value, $match)) {
+            throw new ConvertionException('Value must start with "[" and must end with "]".');
+        }
+
+        return array_filter(
+            array_map('trim', explode(',', $match['values'])),
+            fn ($value) => '' !== $value
+        );
+    }
+}
